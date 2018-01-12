c732e149df46650b8e806426ea0b8526ffcfacef
locnh050792 <techmaster@techmasters-iMac-7.local>
2018-01-12 12:02:32 +0700

    update

--- BT-Kanban/js/js.js
+++ BT-Kanban/js/js.js
@@ -1,16 +1,6 @@
-$(".collection-item").hover(function () {
-    $(this).children().show();
-},
-    function () {
-        $(this).children().hide();
-    });
-
-
-
 var app = {
     newJob: function (e, type, input) {
         var jobName = $(input).val();
-        console.log(jobName);
         var event = window.event || e;
 
         if (event.keyCode === 13 && jobName.trim() !== "") {
@@ -22,33 +12,50 @@ var app = {
     },
 
     addJobToList: function (type, jobName) {
-        console.log(type);
-        var item = '<div href="#!" class="collection-item ui-state-default">' + jobName + '<i class="material-icons";">delete</i>' + '</div>';
+        var item = '<div href="#!" class="collection-item ui-state-default">' + jobName + '<i class="material-icons modal-trigger" data-target="modal1" onclick="app.deleteJob(this)">delete</i>' + '</div>';
         $('#' + type).append(item);
+        $(".collection-item").hover(function () {
+            $(this).children().show();
+        },
+            function () {
+                $(this).children().hide();
+            });
+    },
+
+    deleteJob: function (span) {
+        var modal = $('#modal-confirm');
+        var item = $(span).parent();
+        var btn = $('#btn-delete');
+        $('.modal').modal();
+        modal.modal('open');
+        btn.off('click');
+        btn.on('click', function () {
+            console.log('fc click')
+            item.remove();
+            modal.modal('close');
+        })
     }
 
 }
 
-$('.material-icons').click(function () {
-    $(this).parent().remove();
-})
+$(".connectedSortable").sortable({
+    connectWith: ".connectedSortable",
+    placeholder: "ui-state-highlight",
+
+    start: function (event, ui) {
+        // $(ui.item[0]).addClass('dragging');
+    },
+
+    stop: function (event, ui) {
+        // $(ui.item[0]).addClass('dragging');
+    }
+});
 
 
 $('input').click(function () {
-    // $('label').removeClass('active');
+    $('input').next().removeClass('active');
     $(this).next().addClass('active');
 });
-
-
-$(".sorted-list").sortable({
-    connectWith: ".sorted-list",
-    placeholder: "ui-state-highlight"
-});
-
-    // $(".collection-item").append('<i class="material-icons">delete</i>');
-
-
-
 
 
 
